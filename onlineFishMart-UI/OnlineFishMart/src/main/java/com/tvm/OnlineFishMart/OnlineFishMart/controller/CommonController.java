927e09e4ad616f62d8e4ea611742f8107cc08a22
Yogarani.B <yogarani2014@gmail.com>
2020-06-06 12:33:54 +0530

    User SignUp added

---
+++ onlineFishMart-UI/OnlineFishMart/src/main/java/com/tvm/OnlineFishMart/OnlineFishMart/controller/CommonController.java
@@ -0,0 +1,54 @@
+package com.tvm.OnlineFishMart.OnlineFishMart.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+
+@Api(value = "Fish Mart Controller", description = "REST Apis related to Fish Mart Entity!!!!")
+@org.springframework.web.bind.annotation.RestController
+@CrossOrigin("*")
+
+@Validated
+//use @Validated annotation on top of controller so it is applicable to all methods in it.
+public class CommonController {
+
+
+	private static Logger logger = Logger.getLogger(CommonController.class);
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Index");
+		return "Welcome to Fish Mart Project !!! 8012";
+	}
+
+	
+
+}

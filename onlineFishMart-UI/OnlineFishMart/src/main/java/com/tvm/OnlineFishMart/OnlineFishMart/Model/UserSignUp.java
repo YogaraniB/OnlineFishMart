849289730e1d5439599a5289b00873db516bf012
Yogarani.B <yogarani2014@gmail.com>
2020-06-08 12:13:11 +0530

    resource not found issue fixed

--- onlineFishMart-UI/OnlineFishMart/src/main/java/com/tvm/OnlineFishMart/OnlineFishMart/Model/UserSignUp.java
+++ onlineFishMart-UI/OnlineFishMart/src/main/java/com/tvm/OnlineFishMart/OnlineFishMart/Model/UserSignUp.java
@@ -1,11 +1,17 @@
 package com.tvm.OnlineFishMart.OnlineFishMart.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 public class UserSignUp {
 

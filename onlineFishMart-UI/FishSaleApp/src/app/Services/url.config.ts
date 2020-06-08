877cb615fef9e5bdfdf0a3fe645b3ce354c53935
Yogarani.B <yogarani2014@gmail.com>
2020-06-08 11:31:14 +0530

    url.config.ts added

---
+++ onlineFishMart-UI/FishSaleApp/src/app/Services/url.config.ts
@@ -0,0 +1,12 @@
+// import { environment } from '../../../../src/environments/environment';
+
+/// Don't use underscore variable for links
+/// Always end the links with trailing slash;
+
+export const UrlConfig = {
+  totalUrl:
+
+  {
+      UserSignUps: `api/v1/UserSignUps`
+  }
+};
